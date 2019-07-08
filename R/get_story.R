540d5badfd18b84abfa4e933422dccc841197121
Jan Dix <dix.jan@gmx.de>
2019-07-08 12:11:31 +0200

    update error handling

--- R/get_story.R
+++ R/get_story.R
@@ -4,13 +4,17 @@
 #' one online publication. Each story refers to a single URL from any feed
 #' within a single media source.
 #'
-#' @param story_id Character string that contains a valid sotry id.
+#' @param story_id Positive numeric that contains a valid story id.
 #' @param api_key Character string with the API key you get from mediacloud.org.
 #'                Passing it is compulsory. Alternatively, function can be
 #'                provided from the global environment
 #'                (see \code{set_api_key()}).
 #'
 #' @return Data frame with results. See \url{https://github.com/berkmancenter/mediacloud/blob/master/doc/api_2_0_spec/api_2_0_spec.md#stories} for field descriptions.
+#'
+#' @importFrom httr parse_url build_url GET http_error http_status content
+#' @importFrom jsonlite fromJSON
+#'
 #' @export
 
 get_story <- function(story_id,
@@ -19,6 +23,10 @@ get_story <- function(story_id,
   # errors and warnings --------------------------------------------------------
   # check if story_id is passed
   if (missing(story_id)) stop("Please define a story id.")
+
+  # check if story_id integer and positive
+  if (!is.integer(story_id) | story_id < 0L)
+    stop("Please provide a positive integer for story id.")
 
   # check if api key is passed
   if (nchar(api_key) == 0) {
@@ -42,14 +50,16 @@ get_story <- function(story_id,
   # query and parse api --------------------------------------------------------
   # query api
   response <- httr::GET(url)
-  # check if any error
-  if (httr::http_error(response)) {
-    stop(httr::http_status(response)$message)
-  }
   # parse response
   parsed_response <- httr::content(response, type = "text", encoding = "UTF-8")
   # parse json
   parsed_json <- jsonlite::fromJSON(parsed_response)
+
+  # check possible errors ------------------------------------------------------
+  # check if any error
+  if (httr::http_error(response)) {
+    stop(parsed_json$error)
+  }
 
   # define and return result object --------------------------------------------
   # return result set
