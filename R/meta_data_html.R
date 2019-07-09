563aef6dc214aa2de2a2098c241dede6c7730e53
Jan Dix <dix.jan@gmx.de>
2019-07-09 17:02:07 +0200

    add meta data extraction

---
+++ R/meta_data_html.R
@@ -0,0 +1,6 @@
+#' HTML document to test \code{extract_meta_data}
+#'
+#' A HTML document with basic meta tags for open-graph, twitter and native
+#' meta data.
+#'
+"meta_data_html"
--- tests/testthat/test_extract_meta_data.R
+++ tests/testthat/test_extract_meta_data.R
@@ -8,3 +8,31 @@ testthat::test_that(
   })
 
 # test html document -----------------------------------------------------------
+html_doc <- mediacloudr::meta_data_html
+meta_data <- list(
+  open_graph = list(
+    url = "https://github.com/jandix/mediacloudr",
+    type = "article",
+    title = "Hello world!",
+    image = "https://images.pexels.com/photos/113338/pexels-photo-113338.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    description = "mediacloudr hello world test file"
+  ),
+  twitter = list(
+    url = "https://github.com/jandix/mediacloudr",
+    title = "Hello world!",
+    description = "mediacloudr hello world test file",
+    image = "https://images.pexels.com/photos/113338/pexels-photo-113338.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    image_alt = "Trees",
+    card = "summary_large_image"
+  ),
+  native = list (
+    title = "Hello world!",
+    description = "mediacloudr hello world test file",
+    image = "https://images.pexels.com/photos/113338/pexels-photo-113338.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    thumbnail = "https://images.pexels.com/photos/113338/pexels-photo-113338.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=50"
+  )
+)
+testthat::test_that(
+  "test that function parses the document correct", {
+    testthat::expect_equal(mediacloudr::extract_meta_data(html_doc = html_doc), meta_data)
+  })
